b2f2a22149cd23f13d7a3e1367d3ed289d5cb86f
dothanhtung295 <84143042+dothanhtung295@users.noreply.github.com>
2021-05-13 22:43:52 +0700

    Delete Lab10_1.py

--- Lab10_1.py
+++
@@ -1,8 +0,0 @@
-import time
-from selenium import webdriver
-driver = webdriver.Chrome('D:\ki2nam3\KiemThuPhanMem\Chrome_Driver\chromedriver.exe')
-driver.get('http://practice.automationtesting.in/')
-title = driver.title
-assert title == "Automation Practice Site"
-time.sleep(2)
-driver.quit()
